55cd02d73225ed4a8bc5ba64d154cd0a68098ec5
SabrinaFZ <sabrina.fer.zam@gmail.com>
2019-02-09 21:03:54 +0100

    Add background header

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@
 import './styles/index.scss';
 
 // import scripts
+import './js/api';
 import './js/navbar';
 import './js/beers';
 
 // import images
 import './images/default.jpg';
+import './images/bg-default.jpg';
