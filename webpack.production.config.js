a59f7a42fede9beddc069025c7437da437a9bbf7
SabrinaFZ <sabrina.fer.zam@gmail.com>
2019-02-12 20:25:30 +0100

    configure production webpack config

---
+++ webpack.production.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const webpack = require('webpack');
+const htmlPlugin = require('html-webpack-plugin');
+const cleanWebpackPlugin = require('clean-webpack-plugin');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: path.join(__dirname, 'src', 'index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(jpg|png|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].production.[ext]'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    miniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        new webpack.ProgressPlugin(),
+        new cleanWebpackPlugin(['dist']),
+        new htmlPlugin({
+            template: path.join(__dirname, 'src', 'index.html'),
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true
+            }
+        }),
+        new miniCssExtractPlugin()
+    ]
+};
